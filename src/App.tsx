0b14d83c0c88215aa2cf9ba8d29e23ff4ff46369
paoloperrone <paolo.perrone.math@gmail.com>
2026-06-02 15:43:04 +0100

    fixed scrolling issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { DotsPage } from './pages/DotsPage';
 import { SidecarPage } from './pages/SidecarPage';
+import { ScrollToTop } from './components/ScrollToTop';
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/dots" element={<DotsPage />} />
